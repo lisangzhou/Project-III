7fda8961926b2d6d35858a2aa81a06ee56593b56
Lisang Zhou <Death,2013>
2013-04-28 00:58:07 -0700

    Revert "Revert "Revert "Revert "updating getVertices; addVertex; isVertex; degree""""
    This reverts commit e2a8a986fd58c3d911e503a82dbf8a43b77b939b.

--- graph/WUGraph.java
+++ graph/WUGraph.java
@@ -79,7 +79,9 @@ public class WUGraph {
    *
    * Running time:  O(1).
    */
-  public void addVertex(Object vertex);
+    public void addVertex(Object vertex){
+    
+    }
 
   /**
    * removeVertex() removes a vertex from the graph.  All edges incident on the
@@ -96,7 +98,9 @@ public class WUGraph {
    *
    * Running time:  O(1).
    */
-  public boolean isVertex(Object vertex);
+    public boolean isVertex(Object vertex){
+        
+    }
 
   /**
    * degree() returns the degree of a vertex.  Self-edges add only one to the
