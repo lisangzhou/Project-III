52d5d5a113236af59cbdf10408040a0b628dce66
Eric Wu <wu.eric.g@gmail.com>
2013-04-27 21:50:20 -0700

    added Edge class

---
+++ graph/Edge.java
@@ -0,0 +1,12 @@
+public class Edge{
+
+  DListNode firstAdjacencyNode;
+  DListNode secondAdjacencyNode;
+  int weight;
+
+  public Edge(DListNode first, DListNode second, int edgeWeight){
+    firstAdjacencyNode = first;
+    secondAdjacencyNode = second;
+    weight = edgeWeight;
+  }
+}  
