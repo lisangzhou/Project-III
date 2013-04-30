08eea2df1f33dcf584ca53d1fd7d16ed773ae666
mukundc <mchillakanti@gmail.com>
2013-04-29 18:00:48 -0700

    Update Edge.java

--- graph/Edge.java
+++ graph/Edge.java
@@ -7,26 +7,35 @@ public class Edge{
     protected Vertex origin;
     private int weight;
     private Edge partner;
+    DListNode edgeNode;
     
-    public Edge(Vertex origin, Vertex direction, int edgeWeight){
-        destination=direction;
-        origin=this.origin;
-        weight = edgeWeight;
+    public Edge(Vertex origin, Vertex destination, int weight){
+        this.destination = destination;
+        this.origin = origin;
+        this.weight = weight;
     }
     
-    public int weight(){
-        return this.weight;
+    public int getWeight(){
+        return weight;
     }
     
-    public void weightChange(int a){
-        weight=a;
+    public void setWeight(int w){
+        weight = w;
     }
     
-    public Edge partner(){
-        return this.partner;
+    public Edge getPartner(){
+        return partner;
     }
     
-    public void setPartner(Edge identity){
-        partner=identity;
+    public void setPartner(Edge e){
+        partner = e;
+    }
+    
+    public void setNodeReference(DListNode n) {
+        edgeNode = n;
+    }
+    
+    public DListNode getNodeReference() {
+    	return edgeNode;
     }
 }  
