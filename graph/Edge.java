ec824141f8b9e41a26761b82f2afb232683073b8
mukundc <mchillakanti@gmail.com>
2013-05-02 17:44:31 -0700

    Update Edge.java

--- graph/Edge.java
+++ graph/Edge.java
@@ -3,46 +3,77 @@ import list.*;
 
 
 public class Edge{
-    protected Vertex destination;
-    protected Vertex origin;
+    private Vertex destination;
+    private Vertex origin;
     private int weight;
     private Edge partner;
-    DListNode edgeNode;
+    protected DListNode edgeNode;
     
+    /**
+     *  Edge() constructor
+     *  Creates an Edge and assigns an origin vertex, a destination vertex, and a weight to it.
+     */
     public Edge(Vertex origin, Vertex destination, int weight){
         this.destination = destination;
         this.origin = origin;
         this.weight = weight;
     }
     
+    /**
+     *  getWeight() returns weight of the Edge. 
+     */
     public int getWeight(){
         return weight;
     }
     
-    public void setWeight(int w){
+    /**
+     *  setWeight(int w) sets the weight of the Edge. 
+     *  @param w is the weight
+     */
+    protected void setWeight(int w){
         weight = w;
     }
     
+    /**
+     *  getPartner() returns partner of the Edge. 
+     */
     public Edge getPartner(){
         return partner;
     }
     
-    public void setPartner(Edge e){
+    /**
+     *  setPartner(Edge e) sets the Edge's partner. 
+     *  @param e is the partner.
+     */
+    protected void setPartner(Edge e){
         partner = e;
     }
     
-    public void setNodeReference(DListNode n) {
+    /**
+     *  setNodeReference(DListNode n) sets the edgeNode of the Edge. 
+     *  @param n is the node that the edgeNode is set to.
+     */
+    protected void setNodeReference(DListNode n) {
         edgeNode = n;
     }
     
+    /**
+     *  getNodeReference() returns edgeNode of the Edge. 
+     */
     public DListNode getNodeReference() {
-    	return edgeNode;
+        return edgeNode;
     }
     
+    /**
+     *  getOrigin() returns origin vertex of the Edge. 
+     */
     public Vertex getOrigin() {
     	return origin;
     }
     
+    /**
+     *  getDestination() returns destination vertex of the Edge. 
+     */
     public Vertex getDestination() {
     	return destination;
     }
