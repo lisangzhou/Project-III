c4904b2ce7999a59d423b15aa3a4eb909c00a260
mukundc <mchillakanti@gmail.com>
2013-05-02 17:45:58 -0700

    Update Vertex.java

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -4,28 +4,45 @@ import list.*;
 public class Vertex{
     private DList adjacentEdges;
     private Object item;
-    protected DListNode node;
+    private DListNode node;
 
   
-  public Vertex (Object o){
+    /**
+     *  Vertex() constructor
+     *  Creates a Vertex and assigns a vertex object to its item field.
+     */    
+    public Vertex (Object o){
       item = o;
       adjacentEdges=new DList();
-  }
+    }
     
+    /**
+     *  getItem() returns vertex object of the Vertex. 
+     */
     public Object getItem(){
         return item;
     }
     
+    /**
+     *  getAdjacentEdges() returns a DList of adjacent edges of the Vertex. 
+     */
     public DList getAdjacentEdges(){
         return adjacentEdges;
     }
-
-  public void setNode(DListNode input){
-    this.node = input;
-  }
-
-  public DListNode getNode(){
-    return this.node;
-  }
+    
+    /**
+     *  setNode(DListNode n) sets the node of the Vertex. 
+     *  @param n is the node that the Vertex is assigned to.
+     */
+    protected void setNode(DListNode n) {
+        node = n;
+    }
+    
+    /**
+     *  getNode() returns the node of the Vertex. 
+     */
+    protected DListNode getNode() {
+    	return node;
+    }
     
 }  
